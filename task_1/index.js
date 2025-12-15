1cd6e89a1057cfecd5a61a11b2cfca88871ab129
avatarisCSN <86517706+avatarisCSN@users.noreply.github.com>
2025-12-15 18:03:08 +0200

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -1,7 +1,21 @@
 function convertDistanceUnits(data) {
-  // Implement this function
+  const conversions = {
+    m: 1,
+    cm: 0.01,
+    in: 0.0254,
+    ft: 0.3048,
+  };
 
-  return;
+  const { unit, value } = data.distance;
+  const to = data.convertTo;
+
+  const meters = value * conversions[unit];
+  const result = meters / conversions[to];
+
+  return {
+    unit: to,
+    value: Number(result.toFixed(2)),
+  };
 }
 
 module.exports = {
